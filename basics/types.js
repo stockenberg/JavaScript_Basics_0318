1bf9f5202dda0d65b8aa278ef99108f5ce10054f
Marten Stockenberg <MStockenberg@googlemail.com>
2018-06-28 15:31:57 +0200

    end of lesson 2

---
+++ basics/types.js
@@ -0,0 +1,33 @@
+var _string = 'Hallo welt';
+var _number = 23.45.toString();
+var _bool = true;
+var _undefined;
+var _null = null;
+var _array = [
+  'Wert',
+  2,
+  true,
+  null,
+  [2,3,4,5,6,7,8,9],
+  {key: "value", 1: "test"},
+  undefined
+];
+var _obj = {};
+
+var users = [
+  {
+    personal_data: {
+      name: "Marten",
+      lastname: "Stockenberg",
+      
+    },
+    social_media: {
+      facebook: null,
+      xing: true
+    },
+    portraits: [
+      'img1.jpg',
+      'img2.jpg'
+    ]
+  }
+];
