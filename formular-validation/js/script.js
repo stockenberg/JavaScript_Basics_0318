3d8201ba2cba21ca48663f09ae1408d19f036636
Marten Stockenberg <MStockenberg@googlemail.com>
2018-07-25 13:22:55 +0200

    radio validation try #1

--- formular-validation/js/script.js
+++ formular-validation/js/script.js
@@ -1,4 +1,66 @@
 
 var form = document.forms[0];
+var contact = {};
 
-console.dir(form);
+form.onsubmit = function (ev) {
+    ev.preventDefault();
+    var fields = ev.target;
+
+    for(var i = 0; i < fields.length - 1; i++){
+
+        switch (fields[i].type){
+
+            case 'radio':
+                if(isRequired(fields[i].parentElement)){
+                    var radioIsChecked;
+
+                    var radios = fields[i].parentElement.children;
+                    for(var x = 0; x < radios.length; x++){
+
+                        if(radios[x].tagName === "INPUT"){
+                            if(radios[x].checked){
+                                radioIsChecked = true;
+                            }else{
+                                radioIsChecked = false;
+                            }
+                        }
+                    }
+
+                    console.log("RADIO: " + radioIsChecked);
+                    if(radioIsChecked){
+                        contact['myName'] = fields[i].value;
+                    }
+                }
+                break;
+
+            case 'checkbox':
+                if(isRequired(fields[i])){
+                    if(!isChecked(fields[i])){
+                        console.log("Das " + fields[i].name + " feld ist pflicht...");
+                    }
+                }
+
+                if(isChecked(fields[i])){
+                    contact[fields[i].name] = fields[i].value;
+                }
+
+                console.log(fields[i].checked);
+                break;
+
+            default:
+                console.log(fields[i].value);
+                break;
+        }
+
+    }
+
+    function isRequired(field){
+        return field.dataset.required ? true : false;
+    }
+
+    function isChecked(field){
+        return field.checked;
+    }
+
+    console.log(contact);
+};
