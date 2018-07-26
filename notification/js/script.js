e65d91a6cb02cfd9f5017f78d4dff8516040c996
Marten Stockenberg <MStockenberg@googlemail.com>
2018-07-26 10:25:32 +0200

    beginning of notification to work it over again

---
+++ notification/js/script.js
@@ -0,0 +1,10 @@
+
+message.success('Hallo welt!', background-color);
+message.error('Hallo welt!', background-color);
+message.warning('Hallo welt!', background-color);
+message.info('Hallo welt!', background-color);
+
+
+// 1. Per Event - Eintrag erzeugen
+// 2. Timer - delete
+// last-child child insert
