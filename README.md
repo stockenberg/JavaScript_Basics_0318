ab1f90b87123757543671fedd25584714d3ee49b
Marten Stockenberg <MStockenberg@googlemail.com>
2018-07-26 12:48:51 +0200

    new git commands described

--- README.md
+++ README.md
@@ -5,7 +5,43 @@
 - Notification Plugin
 - Drag and Drop
 - Modal
+- Tabs / Pills
 
+# Git Commands
+- git clone resposity/url.git
+- git add -A
+- git commit -m "your commit message"
+- git push
+
+- git checkout . 
+    - reset your changes
+- git checkout (version_hash)
+    - checks out spezific version of your log
+- git checkout branchname
+- git checkout branchname file/foldername
+    - checkout folder in current branch to merge specific folders
+- git branch branchname
+    - create a new branch
+    
+- git merge branchname
+    - merges the given branchname in current branch
+    
+- git checkout -b branchname 
+    - create and checkout new branch
+    
+- git log
+    - see your log
+    
+- git reset --soft HEAD
+    - resets head an keeps files
+- git reset --hard HEAD
+    - resets head and deletes files
+    
+- git rm file/foldername
+    - deletes and file or folder
+    
+
+    
 
 # 25.07. 
 - Feedback zu Formularvalidierungen
--- accordion/js/script.js
+++ accordion/js/script.js
@@ -4,13 +4,10 @@
  * @constructor
  */
 function Accordion(className){
-
     /** classname of acc open element */
     this._classname = className;
-
     /** if set to true, multiple acc entries can be shown */
     this.multiple = true;
-
     /** Gets all accordion entries and binds click handler */
     this.init = function () {
         window.multiple = this.multiple;
@@ -19,19 +16,16 @@ function Accordion(className){
             entries[i].onclick = this.open;
         }
     };
-
     /** opens acc entries based on multiple settings */
     this.open = function (ev) {
         if(window.multiple){
             ev.target.nextElementSibling.classList.toggle('show');
             return null;
         }
-
         var elements = document.getElementsByClassName('show');
         for(var i = 0; i < elements.length; i++){
             elements[i].classList.remove('show');
         }
-
         ev.target.nextElementSibling.classList.add('show');
     };
 }
