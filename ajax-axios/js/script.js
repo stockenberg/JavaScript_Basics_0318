4a44017f27d9093c305dbe37a681a8e2f7dadae5
Marten Stockenberg <MStockenberg@googlemail.com>
2018-08-01 11:24:29 +0200

    axios fixed

--- ajax-axios/js/script.js
+++ ajax-axios/js/script.js
@@ -1,20 +1,4 @@
-
-
-document.getElementById('cto').onclick = function () {
-    var ajax = new XMLHttpRequest();
-    ajax.onreadystatechange = function () {
-        if(this.readyState === 4 && this.status === 200){
-            var usersList = document.getElementById('userlist');
-            var users = JSON.parse(this.responseText);
-            for(var i = 0; i < users.length; i++){
-                var li = document.createElement('li');
-                li.innerHTML = users[i].username;
-                usersList.appendChild(li);
-            }
-        }
-    };
-
-    ajax.open('GET', 'ajax.json');
-    ajax.send();
-};
-
+axios.get('ajax.json')
+    .then(function(res){
+        console.log(res);
+    });
