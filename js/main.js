8b228d1b185568a5e56e87a30c91fcc15377ad88
Marten Stockenberg <MStockenberg@googlemail.com>
2018-08-09 13:32:05 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
-
+function TestClass () {
+    this.callMe = function () {
+        $('p').html();
+    }
+}
 
 $(document).ready(function () {
 
-    $('h2').click(function (){
-        $(this).next().slideToggle();
-    });
+    $('form').submit(function (ev) {
+
+        var errors = {};
+        ev.preventDefault();
+
+        if($(this).find('input[name="firstname"]').val() === ""){
+            errors.firstname = "Bitte vornamen angeben!";
+        }
+
+        if($(this).find('input[name="lastname"]').val() === ""){
+            errors.lastname = "Bitte lastname angeben!";
+        }
+
+
+        if(!errors.firstname && !errors.lastname){
+            console.log("form submitted");
+            $('body').append('Erfolgreich versandt');
+
+            $.ajax({
+                method: "GET",
+                data: ['my data array'],
+                url: "null"
+            })
+            .success(function(res){
+                console.log(res);
+            })
+        }
+
+
+    })
+
 
 });
