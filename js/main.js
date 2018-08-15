c41d90fbd191f382b37f3f26da936e6d38ef7da2
Marten Stockenberg <MStockenberg@googlemail.com>
2018-08-15 15:40:18 +0200

    cards game

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var deckId = null;
+    var score = 0;
     $('#shuffleDeck').click(function () {
+        score = 0;
         $.ajax({
             method: "GET",
             url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
@@ -10,17 +12,41 @@ $(document).ready(function() {
         })
     })
 
+    var values = {
+        QUEEN: 10,
+        JACK: 10,
+        KING: 10,
+        ACE: 11,
+    };
+
     $('#pullCard').click(function() {
         $.ajax({
             method: "GET",
-            url: "https://deckofcardsapi.com/api/deck/"+deckId+"/draw/?count=5"
+            url: "https://deckofcardsapi.com/api/deck/"+deckId+"/draw/?count=1"
         }).then(function(res){
             console.log(res);
             $('#cards').empty();
-            for(var i = 0; i < res.cards.length; i++){
 
+            for(var i = 0; i < res.cards.length; i++){
+                if(res.cards[i].value.length > 2){
+                    score += parseInt(values[res.cards[i].value]);
+                }else{
+                    score += parseInt(res.cards[i].value);
+                }
                 $('#cards').append('<img src="'+res.cards[i].images.png+'">')
+
             }
+
+            if(score > 21){
+                alert('u lost...!');
+            }
+
+            if(score === 21){
+                alert("Lucky u!");
+            }
+
+            console.log(score);
+
         })
     })
 
