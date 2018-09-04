03e79c82f0d9e672b36c33276aa53b3ef4bc6377
Marten Stockenberg <MStockenberg@googlemail.com>
2018-09-04 17:50:13 +0200

    export / import refined

--- ES6/js/modules/import.js
+++ ES6/js/modules/import.js
@@ -1,3 +1,19 @@
-import {myFunction} from './export.js';
+/**
+ * Import named exports
+ */
+import {myFunction, myFunction2} from './export.js';
+
+/**
+ * Import default export
+ */
+import Test from './export.js';
+
+/**
+ * Call it
+ */
 
 myFunction();
+myFunction2();
+
+var x = new Test();
+x.doIt();
